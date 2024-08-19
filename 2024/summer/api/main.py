f35a1d0b8ec1c690ab973096fa2b21f96c147718
Bryan-Foxy <armandbryan572@icloud.com>
2024-08-19 07:17:17 +0200

    test api good

--- 2024/summer/api/main.py
+++ 2024/summer/api/main.py
@@ -1,6 +1,9 @@
+import os
+import json
 from ner import NER
 from ocr import OCR
 from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 @app.route("/")
@@ -17,15 +20,24 @@ def ocr_and_ner():
         return jsonify({"error": "File not found"}), 400
     
     try:
-        image = file.read()
+        #image = file.read()
+        filename = secure_filename(file.filename)
+        #filepath = os.path.join('uploads', filename)
+        file.save(filename)
         # OCR
-        ocr = OCR(image, lang = ['fr'])
+        ocr = OCR(filename, lang = ['fr'])
         result_ocr = ocr.detection()
 
         # NER
-        ner = NER(result_ocr)
+        tempory_path = "temporary.json"
+        with open(tempory_path, "w") as f:
+            json.dump(result_ocr, f, indent = 4)
+
+        ner = NER(tempory_path)
         result_ner = ner.detection()
-        return jsonify({"result_ocr": result_ocr, "result_ner": result_ner})
+        os.remove(filename)
+        os.remove(tempory_path)
+        return jsonify({"result_ner": result_ner})
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -34,3 +46,5 @@ def ocr_and_ner():
 
 if __name__ == "__main__":
     app.run(debug = True)
+    # Test with curl
+    # curl -X POST -F file=@modele-facture-fr-classique-blanc-750px.png http://127.0.0.1:5000/ocr_and_ner
