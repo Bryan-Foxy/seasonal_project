13eae934e79f5c684fcdeace1df84088f54a97c5
Bryan-Foxy <armandbryan572@icloud.com>
2024-08-23 20:41:27 +0200

    finish

--- 2024/summer/some augmentations techniques/mosaic.py
+++ 2024/summer/some augmentations techniques/mosaic.py
@@ -1,17 +1,57 @@
-"""
-The idea behind Mosaic is very simple. 
-Take 4 images and combine them into a single image. 
-Mosaic does this by resizing each of the four images, stitching them together, and then taking a random cutout of the stitched images to get the final Mosaic image.
-"""
 import cv2
-import torch 
+import numpy as np
+import random
+import albumentations as A
 
-class Mosaic_augmentation:
-    def __init__(self, images, bboxes):
-        super(Mosaic_augmentation, self).__init__()
-        self.images = images
-        self.bboxes = bboxes
-        self.mosaic_images = torch.zeros((self.images.shape, ))
-    
-    def augment(self, resize):
-        return 0
+class Mosaic(A.ImageOnlyTransform):
+    def __init__(self, always_apply=False, p=1.0):
+        super(Mosaic, self).__init__(always_apply, p)
+
+    def apply(self, img, **params):
+        return self.mosaic(img)
+
+    def mosaic(self, img):
+        h, w, _ = img.shape
+        
+        # Randomly generate positions for dividing the image
+        xc, yc = [int(random.uniform(0.4, 0.6) * s) for s in [w, h]]
+        
+        # Create a blank canvas for the mosaic
+        mosaic_img = np.zeros((h, w, 3), dtype=np.uint8)
+        
+        for i in range(4):
+            # Rescale image randomly
+            img_rescaled = cv2.resize(img, (w // 2, h // 2))
+            x_off = (i % 2) * (w // 2)
+            y_off = (i // 2) * (h // 2)
+            
+            # Adjust the image size to fit exactly into the mosaic cell
+            img_rescaled = img_rescaled[:h // 2, :w // 2, :]
+            
+            mosaic_img[y_off:y_off + h // 2, x_off:x_off + w // 2] = img_rescaled
+        
+        return mosaic_img
+
+    def apply_to_bboxes(self, bboxes, **params):
+        # Update bounding boxes according to the new mosaic image
+        mosaic_bboxes = []
+        
+        h, w = params['rows'], params['cols']
+        xc, yc = [int(random.uniform(0.4, 0.6) * s) for s in [w, h]]
+        
+        for bbox in bboxes:
+            x_min, y_min, width, height = bbox[:4]
+            new_bbox = [x_min / 2, y_min / 2, width / 2, height / 2]
+            mosaic_bboxes.append(new_bbox)
+        
+        return mosaic_bboxes
+
+    @property
+    def targets(self):
+        return {
+            "image": self.apply,
+            "bboxes": self.apply_to_bboxes,
+        }
+
+    def get_params(self):
+        return {}
